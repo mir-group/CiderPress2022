1e62c81050407bd3365293c749db9c715479038c
kylebystrom <kylebystrom@g.harvard.edu>
2020-02-04 15:28:43 -0500

    a couple minor fixes

--- scripts/add_polarized_deltau.py
+++ scripts/add_polarized_deltau.py
@@ -1,7 +1,7 @@
 import os
 
 for root, dirs, files in os.walk(os.environ['MLDFTDB']):
-    if ('UHF' in root or 'UKS' in root) and 'data.hdf5' in files:
+    if ('UHF' in root or 'UKS' in root or 'UCCSD' in root) and 'data.hdf5' in files:
         fname = os.path.join(root, 'data.hdf5')
         old_size = os.path.getsize(fname)
         analyzer = UHFAnalyzer.load(fname, max_mem = 10)
