d6e35d135643c5453e9a11ed5ded3adc578e53f2
kylebystrom <kylebystrom@berkeley.edu>
2020-02-17 05:59:05 -0800

    move get_dft_data and company to pyscf_utils and start making helper functions to get individual descriptors

--- mldftdat/pyscf_utils.py
+++ mldftdat/pyscf_utils.py
@@ -423,3 +423,76 @@ def load_calc(fname):
 def load_analyzer_data(fname):
     data_file = os.path.join(dirname, fname)
     return lib.chkfile.load(data_file, 'analyzer/data')
+
+def get_ws_radii(rho):
+    return (3.0 / (4 * np.pi * rho + 1e-7))**(1.0/3)
+
+def get_gradient_magnitude(rho_data):
+    return np.linalg.norm(rho_data[1:4,:], axis=0)
+
+def get_normalized_grad(rho, mag_grad):
+    sprefac = 2 * (3 * np.pi * np.pi)**(1.0/3)
+    n43 = rho**(4.0/3)
+    s = mag_grad / (sprefac * n43 + 1e-7)
+    return s
+
+def get_single_orbital_tau(rho, mag_grad):
+    return mag_grad**2 / (8 * rho + 1e-7)
+
+def get_uniform_tau(rho):
+    return (3.0/10) * (3*np.pi**2)**(2.0/3) * rho**(5.0/3)
+
+def get_normalized_tau(tau, tau_w, tau_unif):
+    return (tau - tau_w) / (tau_unif + 1e-6)
+
+def get_dft_input(rho_data):
+    rho = rho_data[0,:]
+    r_s = get_ws_radii(rho)
+    mag_grad = get_gradient_magnitude(rho_data)
+    s = get_normalized_grad(rho, mag_grad)
+    tau_w = get_single_orbital_tau(rho, mag_grad)
+    tau_unif = get_uniform_tau(rho)
+    alpha = get_normalized_tau(rho_data[5], tau_w, tau_unif)
+    return rho, s, alpha, tau_w, tau_unif
+
+def get_nonlocal_data(rho_data, tau_data, ws_radii, coords, weights):
+    vals = []
+
+    for i in range(weights.shape[0]):
+        ws_radius = ws_radii[i]
+        vecs = coords - coords[i]
+        rs = np.linalg.norm(vecs, axis=1)
+        exp_weights = np.exp(- rs / ws_radius)
+        drho = rho_data[1:4,:]
+        dvh = np.dot(rho_data[1:4,:] / rs, weights)
+        # r dot nabla rho
+        rddrho = np.dot(vecs, drho)
+        # r dot nabla v_ha
+        rddvh = np.dot(vecs, dvh)
+        rddvh_int = np.dot(weights, rddvh)
+        rddrho_int = np.dot(weights, rddrho)
+        dtau = tau_data[1:4,:]
+        # r dot nabla tau
+        rddtau = np.dot(vecs, dtau)
+        rddtau_int = np.dot(weights, rddtau)
+
+        vals.append([np.linalg.norm(dvh, axis=0), rddvh_int, rddrho_int, rddtau_int])
+
+    return np.array(vals).tranpose()
+
+def squish_density(rho_data, coords, weights, alpha):
+    new_coords = coords / alpha
+    new_weights = weights / alpha**3
+    rho_data = rho_data.copy()
+    rho_data[0,:] *= alpha**3
+    rho_data[1:4,:] *= alpha**4
+    rho_data[4:6,:] *= alpha**5
+    return new_coords, new_weights, rho_data
+
+def regularize_nonlocal_data(nonlocal_data, rho, s, alpha):
+    sprefac = 2 * (3 * np.pi * np.pi)**(1.0/3)
+    n43 = rho**(4.0/3)
+    nonlocal_data[0,:] /= tau_unif
+    nonlocal_data[1,:] /= (sprefac * n43)
+    nonlocal_data[3,:] /= tau_unif
+    return nonlocal_data
