ffcd9fc8c4625b0c5673e23e22620e0e1eac3c00
kylebystrom <kylebystrom@g.harvard.edu>
2020-03-14 12:26:10 -0400

    minor rearrangements

--- scripts/collect_x_qm9.py
+++ scripts/collect_x_qm9.py
@@ -1,5 +1,5 @@
 #from mldftdat.gp import DFTGP
-from mldftdat.data import get_unique_coord_indexes_spherical
+from mldftdat.data import get_unique_coord_indexes_spherical, compile_dataset
 from mldftdat.density import get_exchange_descriptors
 from mldftdat.lowmem_analyzers import RHFAnalyzer
 from mldftdat.workflow_utils import get_save_dir
@@ -16,36 +16,6 @@ print(MOL_IDS)
 #exit()
 BASIS = 'aug-cc-pvtz'
 
-all_descriptor_data = None
-all_values = []
 
-for MOL_ID in MOL_IDS:
-    print('Working on {}'.format(MOL_ID))
-    data_dir = get_save_dir(SAVE_ROOT, CALC_TYPE, BASIS, MOL_ID, FUNCTIONAL)
-    print('load analyzer')
-    analyzer = RHFAnalyzer.load(data_dir + '/data.hdf5')
-    print('get descriptors')
-    descriptor_data = get_exchange_descriptors(analyzer.rho_data,
-                                               analyzer.tau_data,
-                                               analyzer.grid.coords,
-                                               analyzer.grid.weights,
-                                               restricted = True)
-    values = analyzer.get_fx_energy_density()
-    print(np.min(values))
-    descriptor_data = descriptor_data
-    print(np.max(descriptor_data[0]))
-    print(values.shape, descriptor_data.shape)
-    if all_descriptor_data is None:
-        all_descriptor_data = descriptor_data
-    else:
-        all_descriptor_data = np.append(all_descriptor_data, descriptor_data,
-                                        axis = 1)
-    all_values = np.append(all_values, values)
+compile_dataset('qm9_x_aug', MOL_IDS, SAVE_ROOT, CALC_TYPE, FUNCTIONAL, BASIS, RHFAnalyzer)
 
-print(all_descriptor_data.shape, all_values.shape)
-save_dir = os.path.join(SAVE_ROOT, 'DATASETS')
-desc_file = os.path.join(save_dir, 'qm9_x_rho_data.npz')
-fx_file = os.path.join(save_dir, 'qm9_x_fx.npz')
-np.savetxt(desc_file, all_descriptor_data)
-np.savetxt(fx_file, all_values)
-#gp = DFTGP(descriptor_data, values, 1e-3)
