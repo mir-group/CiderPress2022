69640a9281553148cd00d49d7e81f8403ffbc18b
kylebystrom <kylebystrom@berkeley.edu>
2020-03-18 13:06:59 -0700

    implementation of fast localized exchange energy density using new int3c2e_lhpot_sph integral in libcint

--- mldftdat/loc_analyzers.py
+++ mldftdat/loc_analyzers.py
@@ -38,53 +38,26 @@ def get_centered_mols(mol, center):
     mol2.build()
     return mol1, mol2
 
-def get_eri(mol, center):
-    mol1, mol2, = get_centered_mols(mol, center)
-    phases = []
-    for j in range(mol1.nbas):
-        l = mol1._bas[j][1]
-        phases += [(-1)**l] * (2*l+1)
-    phases = np.array(phases)
-    atm, bas, env = \
-        gto.mole.conc_env(mol1._atm, mol1._bas, mol1._env,
-                          mol2._atm, mol2._bas, mol2._env)
-    # https://github.com/pyscf/pyscf/blob/master/examples/gto/21-concatenate_molecules.py
-    res = gto.moleintor.getints('cint1e_rinv_sph', atm, bas, env, shls_slice=(0, mol1.nbas, mol1.nbas, mol1.nbas + mol2.nbas))
-    #print(phases, phases.shape, res.shape)
-    for i in range(mol1.nao_nr()):
-        res[i,:] *= phases[i]
-    return res
-    """
-    eri2c = np.zeros((mol1.nao_nr(), mol2.nao_nr()))
-    pi = 0
-    for i in range(mol1.nbas):
-        pj = 0
-        for j in range(mol1.nbas, mol1.nbas+mol2.nbas):
-            shls = (i, j)
-            buf = gto.moleintor.getints_by_shell('cint1e_rinv_sph',
-                                                 shls, atm, bas, env)
-            di, dj = buf.shape
-            #print(buf.shape)
-            eri2c[pi:pi+di,pj:pj+dj] = buf
-            pj += dj
-        pi += di
-    return eri2c
-    """
-
 def get_aux_mat_chunks(mol, points, num_chunks):
-    naux = mol.nao_nr()
+    """
+    Generate chunks of vele_mat on the fly to reduce memory load.
+    """
+    phases = []
+    for j in range(mol.nbas):
+        l = mol._bas[j][1]
+        phases += [(-1)**l] * (2*l+1)
+    phase = np.array(phases)
     num_pts = points.shape[0]
     for i in range(num_chunks):
         start = (i * num_pts) // num_chunks
-        end = min(((i+1) * num_pts) // num_chunks, num_pts)
-        chunk = np.zeros((end - start, naux, naux))
-        for index in range(start, end):
-            center = points[index]
-            #mol2 = invert_mol(mol, center)
-            #mol3 = gto.fakemol_for_charges(np.array([center]))
-            #chunk[index-start,:,:] = df.incore.aux_e2(mol + mol2, mol3)[:naux,naux:,0]
-            chunk[index-start,:,:] = get_eri(mol, points[index])
-        yield chunk
+        end = ((i+1) * num_pts) // num_chunks
+        auxmol = gto.fakemol_for_charges(points[start:end])
+        vele_mat_chunk = df.incore.aux_e2(mol, auxmol, intor='int3c2e_lhpot_sph')
+        vele_mat_chunk = np.ascontiguousarray(np.transpose(
+                                vele_mat_chunk, axes=(2,0,1)))
+        for i in range(mol.nao_nr()):
+            vele_mat_chunk[:,:,i] *= phases[i]
+        yield vele_mat_chunk
 
 def get_aux_mat_generator(mol, coords, num_chunks):
     return lambda: get_aux_mat_chunks(mol, coords, num_chunks)
@@ -96,8 +69,8 @@ def get_fx_energy_density_from_aug(aux_mat_gen, mo_to_aux,
     fx_energy_density = np.array([])
     i = 0
     for aux_mat_chunk in aux_mat_gen():
-        print('iter', i)
-        i += 1
+        #print('iter', i)
+        #i += 1
         fx_energy_density = np.append(fx_energy_density,
                             np.einsum('pq,xpq->x', ex_dens, aux_mat_chunk))
     return -fx_energy_density
@@ -106,7 +79,7 @@ class RHFAnalyzer(lowmem_analyzers.RHFAnalyzer):
 
     def setup_etb(self):
         #auxbasis = df.aug_etb(mol, beta=1.6)
-        auxbasis = df.aug_etb(self.mol, beta=2.3)
+        auxbasis = df.aug_etb(self.mol, beta=1.6)
         nao = self.mol.nao_nr()
         self.auxmol = df.make_auxmol(self.mol, auxbasis)
         naux = self.auxmol.nao_nr()
@@ -137,7 +110,7 @@ class RHFAnalyzer(lowmem_analyzers.RHFAnalyzer):
         #                            self.auxmol, self.grid.coords,
         #                            self.aux_num_chunks)
         small_grid = Grids(self.mol)
-        small_grid.level = 1
+        small_grid.level = 3
         small_grid.build()
         self.small_grid = small_grid
         self.mo_aux_mat_generator = get_aux_mat_generator(
