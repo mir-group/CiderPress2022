4e06cece1ac50bde1a1622993d7eb301ff1c09bc
kylebystrom <kylebystrom@g.harvard.edu>
2020-06-01 09:40:36 -0400

    clean up neural network run script and module

--- mldftdat/models/nn.py
+++ mldftdat/models/nn.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
-A = 0.704 # maybe replace with sqrt(6/5)?
+A = torch.tensor(0.704) # maybe replace with sqrt(6/5)?
 B = 2 * np.pi / 9 * np.sqrt(6.0/5)
 FXP0 = 27 / 50 * 10 / 81
 FXI = 1.0 / 3 * (4*np.pi**2 / 3)**(1.0/3)
@@ -12,19 +13,35 @@ C2 = 1 - C1
 C3 = 0.19697 * np.sqrt(0.704)
 C4 = (C3**2 - 0.09834 * MU) / C3**3
 
+def asinh(x):
+    return torch.log(x+(x**2+1)**0.5)
+
 def edmgga_from_q(Q):
-    x = A * Q + np.sqrt(1 + (A*Q)**2)
-    FX = C1 + (C2 * x) / (1 + C3 * torch.sqrt(x) * torch.arcsinh(C4 * (x-1)))
+    x = A * Q + torch.sqrt(1 + (A*Q)**2)
+    FX = C1 + (C2 * x) / (1 + C3 * torch.sqrt(x) * asinh(C4 * (x-1)))
     return FX
 
 class PolyAnsatz(nn.Module):
 
     def __init__(self, ndesc, func = edmgga_from_q, quadratic = False):
+        super(PolyAnsatz, self).__init__()
         self.func = edmgga_from_q
         if quadratic:
             ndesc = ndesc**2
         self.linear = nn.Linear(ndesc, 1, bias = True)
         self.quadratic = quadratic
+        with torch.no_grad():
+            print(self.linear.weight.size())
+            print(self.linear.bias.size())
+            #self.linear.weight = nn.Parameter(torch.tensor([[0.0, -1.0, 0.25, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0][:ndesc]], dtype=torch.float64))
+            self.linear.weight = nn.Parameter(torch.tensor([[-0.2765, -0.9667,  0.2315, -0.0504,\
+                                                            0.0052, -0.0024,  0.0046,  0.0142, 0.0555,\
+                                                            0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0][:ndesc]],\
+                                                            dtype=torch.float64))
+            #self.linear.bias = nn.Parameter(torch.tensor([1.0], dtype=torch.float64))
+            self.linear.bias = nn.Parameter(torch.tensor([1.0372], dtype=torch.float64))
+            print(self.linear.weight.size())
+            print(self.linear.bias.size())
 
     def forward(self, x):
         if self.quadratic:
@@ -32,9 +49,9 @@ class PolyAnsatz(nn.Module):
             tind = x.triu().nonzero().transpose()
             x = x[tind[0], tind[1]]
         x = self.linear(x)
-        return self.func(x)
+        return torch.squeeze(self.func(x), dim=1)
 
-def get_traing_obj(lr = 0.01):
+def get_traing_obj(model, lr = 0.01):
     criterion = nn.MSELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr = lr)
     return criterion, optimizer
@@ -42,20 +59,20 @@ def get_traing_obj(lr = 0.01):
 def train(x, y_true, criterion, optimizer, model):
     model.train()
     x = torch.tensor(x)
-    y = torch.tensor(y)
+    y_true = torch.tensor(y_true)
     optimizer.zero_grad()
     y_pred = model(x)
     loss = criterion(y_pred, y_true)
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
     optimizer.step()
-    print('Current Train Loss', loss.item())
+    #print('Current Train Loss', loss.item())
     return loss.item()
 
 def validate(x, y_true, criterion, model):
     model.eval()
     x = torch.tensor(x)
-    y = torch.tensor(y)
+    y_true = torch.tensor(y_true)
     y_pred = model(x)
     loss = criterion(y_pred, y_true)
     print('Current Train Loss', loss.item())
