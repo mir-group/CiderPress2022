2fca4335a719b1065acec4ce12f8eefb9051cbbe
kylebystrom <kylebystrom@g.harvard.edu>
2020-06-06 14:22:54 -0400

    develop a working neural net model that gets reasonable performance

--- mldftdat/models/nn.py
+++ mldftdat/models/nn.py
@@ -21,12 +21,33 @@ def edmgga_from_q(Q):
     FX = C1 + (C2 * x) / (1 + C3 * torch.sqrt(x) * asinh(C4 * (x-1)))
     return FX
 
+def edmgga_from_q_param(Q, C3param, C4param):
+    x = A * Q + torch.sqrt(1 + (A*Q)**2)
+    FX = C1 + (C2 * x) / (1 + C3param * torch.sqrt(x) * asinh(C4param * (x-1)))
+    return FX
+
+class Predictor():
+
+    def __init__(self, model, get_descriptors, num, y_to_xed):
+        self.model = model
+        self.get_descriptors = get_descriptors
+        self.num = num
+        self.y_to_xed = y_to_xed
+        self.model.eval()
+
+    def predict(self, X, rho_data):
+        self.model.eval()
+        with torch.no_grad():
+            X = torch.tensor(self.get_descriptors(X, rho_data, self.num))
+            F = self.model(X).numpy()
+        return self.y_to_xed(F, rho_data)
+
 class PolyAnsatz(nn.Module):
 
-    def __init__(self, ndesc, func = edmgga_from_q, quadratic = False,
-                 init_weight = None, init_bias = 1.0):
+    def __init__(self, ndesc, func = edmgga_from_q_param, quadratic = False,
+                 init_weight = None, init_bias = 1.0, C3init = C3, C4init = C4):
         super(PolyAnsatz, self).__init__()
-        self.func = edmgga_from_q
+        self.func = func
         if quadratic:
             ndesc = ndesc + ndesc * (ndesc + 1) // 2
         self.linear = nn.Linear(ndesc, 1, bias = True)
@@ -39,23 +60,31 @@ class PolyAnsatz(nn.Module):
                 init_weight = np.array(init_weight)
                 size = init_weight.shape[0]
                 weight[:size] = init_weight
+            else:
+                weight[0] = 0.0
+                weight[1] = -1.0
+                weight[2] = 0.25
             self.linear.weight = nn.Parameter(torch.tensor([weight],
                                                 dtype=torch.float64))
             self.linear.bias = nn.Parameter(torch.tensor([init_bias], dtype=torch.float64))
             print(self.linear.weight.size())
             print(self.linear.bias.size())
+        self.C3param = nn.Parameter(torch.tensor(C3init, dtype=torch.float64))
+        self.C4param = nn.Parameter(torch.tensor(C4init, dtype=torch.float64))
 
     def forward(self, x):
         if self.quadratic:
-            x2 = torch.ger(x, x)
-            tind = x2.triu().nonzero().transpose()
-            x = torch.cat([x, x2[tind[0], tind[1]]], dim=1)
+            x2 = torch.einsum('bi,bj->bij', x, x)
+            tind = torch.transpose(x2[0,:,:].triu().nonzero(), 0, 1)
+            #print(tind.size())
+            x = torch.cat([x, x2[:, tind[0], tind[1]]], dim=1)
         x = self.linear(x)
-        return torch.squeeze(self.func(x), dim=1)
+        return torch.squeeze(self.func(x, self.C3param, self.C4param), dim=1)
 
-def get_traing_obj(model, lr = 0.01):
+def get_traing_obj(model, lr = 0.005):
     criterion = nn.MSELoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+    #optimizer = torch.optim.SGD(model.parameters(), lr = lr)
+    optimizer = torch.optim.LBFGS(model.parameters(), lr = lr, max_iter = 2000, history_size=2000)
     return criterion, optimizer
 
 def train(x, y_true, criterion, optimizer, model):
@@ -67,7 +96,15 @@ def train(x, y_true, criterion, optimizer, model):
     loss = criterion(y_pred, y_true)
     loss.backward()
     torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-    optimizer.step()
+    #optimizer.step()
+    def closure():
+        optimizer.zero_grad()
+        y_pred = model(x)
+        loss = criterion(y_pred, y_true)
+        #print('loss:', loss.item())
+        loss.backward()
+        return loss
+    optimizer.step(closure)
     #print('Current Train Loss', loss.item())
     return loss.item()
 
@@ -83,7 +120,7 @@ def validate(x, y_true, criterion, model):
 def save_nn(model, fname):
     torch.save(model.state_dict(), fname)
 
-def load_nn(fname, ndesc, func = edmgga_from_q, quadratic = False):
+def load_nn(fname, ndesc, func = edmgga_from_q_param, quadratic = False):
     model = PolyAnsatz(ndesc, func, quadratic)
     model.load_state_dict(torch.load(fname))
     model.eval()
--- scripts/train_nn.py
+++ scripts/train_nn.py
@@ -17,8 +17,37 @@ def xed_to_y(xed, rho_data):
     return xed / (ldax(rho_data[0,:]) - 1e-7)
 
 def get_descriptors(X, rho_data, num = 1):
+    #X[:,1] = X[:,1]**2
+    #return X[:,(1,2,3,4,5,8,6,12,15,16,13,14)[:num]]
+    fac = (6 * np.pi**2)**(2.0/3) / (16 * np.pi)
     X[:,1] = X[:,1]**2
-    return X[:,(1,2,3,4,5,8,6,12,15,16,13,14)[:num]]
+    p = X[:,1]
+    alpha = X[:,2]
+    nabla = X[:,3]
+    scale = np.sqrt(1 + fac * p + 0.6 * fac * (alpha - 1)) # 4^(1/3) for 16, 1/(4)^(1/3) for 15
+    desc = np.zeros((X.shape[0], 27))
+    desc[:,0] = X[:,4] * scale
+    desc[:,1] = X[:,4] * scale**3
+    desc[:,2] = X[:,4] * scale**5
+    desc[:,3] = X[:,5] * scale
+    desc[:,4] = X[:,5] * scale**3
+    desc[:,5] = X[:,5] * scale**5
+    desc[:,6] = np.sqrt(X[:,8]) * scale
+    desc[:,7] = np.sqrt(X[:,8]) * scale**3
+    desc[:,8] = np.sqrt(X[:,8]) * scale**5
+    desc[:,9] = X[:,15] * scale
+    desc[:,10] = X[:,15] * scale**3
+    desc[:,11] = X[:,15] * scale**5
+    desc[:,12] = X[:,16] * scale
+    desc[:,13] = X[:,16] * scale**3
+    desc[:,14] = X[:,16] * scale**5
+    desc[:,15] = p**2
+    desc[:,16] = p * alpha
+    desc[:,17] = alpha**2
+    desc[:,18:21] = desc[:,0:3]**2
+    desc[:,21:24] = desc[:,9:12]**2
+    desc[:,24:27] = desc[:,12:15]**2
+    return np.append(X[:,1:4], desc, axis=1)[:,:num] 
 
 dir1 = os.path.join(SAVE_ROOT, 'DATASETS', 'atoms_x2')
 dir2 = os.path.join(SAVE_ROOT, 'DATASETS', 'augG2_x2_30_50')
@@ -50,12 +79,24 @@ y_train = np.append(y_train, y_test[::200], axis=0)
 rho_train = np.append(rho_train, rho_test[::200], axis=0)
 rho_data_train = np.append(rho_data_train, rho_data_test[:,::200], axis=1)
 
-init_weight = np.array([-0.2888, -0.9707,  0.2324, -0.0547,
-                        0.0075, -0.0024,  0.0051,  0.0141, 0.0551])
-init_bias = 0.9241
-model = PolyAnsatz(NUM)
+init_weight = np.array([-0.3878, -1.2680,  0.2871,  0.0489, -0.1944,  0.2042, -0.0493,  0.0099,\
+         -0.0829,  0.1251, -0.1165,  0.0350, -0.0320,  0.1202, -0.0364,  0.1601,\
+         -0.2346,  0.0449,  0.0521,  0.1546, -0.0187])
+
+init_weight = np.array([-3.8867e-01, -1.3832e+00,  2.7172e-01,  7.8060e-02, -1.2923e-01,\
+          2.8096e-01, -5.6653e-02,  3.9503e-02, -1.0942e-01,  1.2630e-01,\
+         -8.1625e-02,  3.0215e-02, -7.3405e-02,  1.2056e-01, -4.1592e-02,\
+          1.7762e-01, -2.0912e-01,  8.8963e-02,  2.5596e-02,  1.1125e-01,\
+         -7.2691e-03, -1.5155e-02, -3.1665e-03,  6.5592e-05])
+init_bias = 1.1956
+init_bias = 1.1176
+#init_bias = np.sqrt(3.7)
+model = PolyAnsatz(NUM, quadratic = False, init_weight = init_weight, init_bias = init_bias,
+                   C3init=0.2054, C4init=1.4396)
+                   #C3init=0.2078, C4init=1.4731)
+#model = PolyAnsatz(NUM)
 model.double()
-criterion, optimizer = get_traing_obj(model)
+criterion, optimizer = get_traing_obj(model, lr=0.0002)
 converged = False
 i = 0
 X_train = get_descriptors(X_train, rho_data_train, num = NUM)
@@ -64,15 +105,17 @@ y_train = xed_to_y(y_train, rho_data_train)
 y_val = xed_to_y(y_test[4::100], rho_data_test[:,4::100])
 while not converged:
     loss = train(X_train, y_train, criterion, optimizer, model)
-    if i % 1000 == 0:
+    if i % 100 == 0:
         print('Current Train Loss', loss)
+        print(validate(X_val, y_val, criterion, model))
     i += 1
     if loss < 0.0001:
         converged = True
-    if i > 100000:
+    if i > 3000:
         break
 print(converged, np.sqrt(loss))
 print(validate(X_val, y_val, criterion, model))
 save_nn(model, 'nnmodel_%d.pth' % NUM)
 print(model.linear.weight)
 print(model.linear.bias)
+print(model.C3param, model.C4param)
