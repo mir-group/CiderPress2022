066fd188aebeda0b12b52bdaec98cf4144dd9ee6
kylebystrom <kylebystrom@g.harvard.edu>
2020-06-26 08:04:01 -0400

    refinements to nn model

--- scripts/train_nn4.py
+++ scripts/train_nn4.py
@@ -26,10 +26,10 @@ dir4 = os.path.join(SAVE_ROOT, 'DATASETS', 'spinpol_atoms_x2')
 X_train, y_train, rho_data_train, wttrain = load_descriptors(dir1, load_wt = True, val_dirname = dir1.replace('x2', 'locx86'))
 X_train2, y_train2, rho_data_train2, wttrain2 = load_descriptors(dir4, load_wt = True, val_dirname = dir4.replace('x2', 'locx86'))
 print(y_train.shape, y_train2.shape, wttrain.shape, wttrain2.shape)
-X_train = np.append(X_train, X_train2[::100], axis=0)
-y_train = np.append(y_train, y_train2[::100], axis=0)
-wttrain = np.append(wttrain, wttrain2[::100], axis=0)
-rho_data_train = np.append(rho_data_train, rho_data_train2[:,::100], axis=1)
+X_train = np.append(X_train, X_train2[::50], axis=0)
+y_train = np.append(y_train, y_train2[::50], axis=0)
+wttrain = np.append(wttrain, wttrain2[::50], axis=0)
+rho_data_train = np.append(rho_data_train, rho_data_train2[:,::50], axis=1)
 X_train, y_train, rho_train, rho_data_train, wttrain = filter_descriptors(
         X_train, y_train, rho_data_train, tol=3e-3, wt=wttrain)
 X_test, y_test, rho_data_test, wttest = load_descriptors(dir2, load_wt = True, val_dirname = dir2.replace('x2', 'locx86'))
@@ -46,38 +46,41 @@ rho_data_test = np.append(rho_data_test, rho_data_test3, axis=1)
 X_test, y_test, rho_test, rho_data_test, wttest = filter_descriptors(
         X_test, y_test, rho_data_test, tol=3e-3, wt=wttest)
 
-X_train = np.append(X_train, X_test[::200], axis=0)
-y_train = np.append(y_train, y_test[::200], axis=0)
-rho_train = np.append(rho_train, rho_test[::200], axis=0)
-rho_data_train = np.append(rho_data_train, rho_data_test[:,::200], axis=1)
-wttrain = np.append(wttrain, wttest[::200], axis=0)
+X_train = np.append(X_train, X_test[::100], axis=0)
+y_train = np.append(y_train, y_test[::100], axis=0)
+rho_train = np.append(rho_train, rho_test[::100], axis=0)
+rho_data_train = np.append(rho_data_train, rho_data_test[:,::100], axis=1)
+wttrain = np.append(wttrain, wttest[::100], axis=0)
 
-train_weights = LDA_FACTOR * rho_train**(4.0 / 3) * wttrain
+train_weights = LDA_FACTOR * rho_train**(1.0 / 3) * wttrain
 
+print(np.isnan(X_train).any(), np.isnan(y_train).any(), (y_train > 0).any(),
+      np.isnan(rho_train).any(), (rho_train < 0).any())
 X_train = get_desc2(X_train)
-y_train = get_y_from_xed(y_train, rho_data_train)
+y_train = get_y_from_xed(y_train, rho_train)
 
-model = BayesianLinearFeat(48, 8, X_train, y_train, train_weights, order = 2)
+print(X_train.shape, y_train.shape, train_weights.shape)
+model = BayesianLinearFeat(48, 8, X_train[::2], y_train[::2].reshape(-1,1), train_weights[::2].reshape(-1,1), order = 3)
 #model = PolyAnsatz(NUM)
 model.double()
-criterion, optimizer = get_traing_obj(model, lr=0.02)
+criterion, optimizer = get_training_obj(model, lr=0.1)
 converged = False
 i = 0
 X_val = get_desc2(X_test[4::100])
-y_val = get_y_from_xed(y_test[4::100], rho_data_test[:,4::100])
+y_val = get_y_from_xed(y_test[4::100], rho_test[4::100])
 while not converged:
-    loss = train(X_val[::2], y_val[::2], criterion, optimizer, model)
-    if i % 100 == 0:
+    loss = train(X_train[1::2], y_train[1::2].reshape(-1,1), criterion, optimizer, model)
+    if i % 10 == 0:
         print('Current Train Loss', loss)
-        print(validate(X_val[::2], y_val[::2], criterion, model))
+        print(validate(X_val, y_val.reshape(-1,1), criterion, model))
     i += 1
     if loss < 0.0001:
         converged = True
-    if i > 3000:
+    if i > 300:
         break
 print(converged, np.sqrt(loss))
-print(validate(X_val, y_val, criterion, model))
-save_nn(model, 'nnmodel_%d.pth' % NUM)
-print(model.linear.weight)
-print(model.linear.bias)
+print(np.sqrt(validate(X_val, y_val.reshape(-1,1), criterion, model)))
+save_nn(model, 'nnmodel_cubic.pth')
+#print(model.linear.weight)
+#print(model.linear.bias)
 
