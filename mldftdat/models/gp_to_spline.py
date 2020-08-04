448625b0c6c0cdfd7b3996bdf2191c88eea47171
kylebystrom <kylebystrom@berkeley.edu>
2020-08-04 10:51:03 -0700

    gp_to_spline for correlation

--- mldftdat/models/gp_to_spline.py
+++ mldftdat/models/gp_to_spline.py
@@ -100,6 +100,26 @@ class Evaluator():
                    state_dict['get_descriptors'],
                    state_dict['num'])
 
+
+class Evaluator2(Evaluator):
+
+    def __init__(self, scale, ind_sets, spline_grids, coeff_sets,
+                 y_to_xed, get_descriptors, num, const = 0.0):
+        self.scale = scale
+        self.nterms = len(self.scale)
+        self.ind_sets = ind_sets
+        self.spline_grids = spline_grids
+        self.coeff_sets = coeff_sets
+        self.y_to_xed = y_to_xed
+        self.get_descriptors = get_descriptors
+        self.num = num
+        self.const = const
+
+    def predict_from_desc(self, X, max_order = 3, vec_eval = False, subind = 0):
+        return self.const + super(self, Evaluator2).predict_from_desc(
+                                X, max_order, vec_eval, subind)
+
+
 def get_dim(x, length_scale, density = 6, buff = 0.0, bound = None):
     mini = np.min(x) - buff
     maxi = np.max(x) + buff
@@ -109,11 +129,23 @@ def get_dim(x, length_scale, density = 6, buff = 0.0, bound = None):
     ngrid = int(density * ran / length_scale) + 1
     return (mini, maxi, ngrid)
 
-def get_mapped_gp_evaluator(gpr, test_x = None, test_y = None, test_rho_data = None):
+def get_mapped_gp_evaluator(gpr, test_x = None, test_y = None, test_rho_data = None,
+                            version = 'a'):
+    """
+    version a is for RBF(s) * ARBF(other desc) with X[:,0]=rho (used for exchange)
+    version b is for Linear(SCAN) * ARBF(other desc) with X[:,0]=rho
+    (used for correlation)
+    """
     X = gpr.X
     alpha = gpr.gp.alpha_
-    d0 = X[:,1]
-    d1 = X[:,2:]
+    if version == 'a'
+        d0 = X[:,1]
+        d1 = X[:,2:]
+    elif version == 'b':
+        d0 = np.ones(X.shape[0])
+        d1 = X[:,2:]
+    else:
+        raise ValueError('version must be a or b')
     print("SHAPES", d0.shape, d1.shape)
     y = gpr.y
     print(gpr.gp.kernel_)
@@ -173,6 +205,120 @@ def get_mapped_gp_evaluator(gpr, test_x = None, test_y = None, test_rho_data = N
         coeff_sets.append(filter_cubic(spline_grids[i], funcps[i]))
     evaluator = Evaluator(scale, ind_sets, spline_grids, coeff_sets,
                           gpr.y_to_xed, gpr.get_descriptors, gpr.num)
+    if not isinstance(aqrbf, RBF):
+        return evaluator
+
+    res, en = aqrbf(X, get_sub_kernels = True)
+    resg = gradk(X)
+    res = np.dot(alpha, aqrbf.scale[0] * resg)
+    print("Comparing K and Kspline!!!")
+    print(en[0])
+    tsp = eval_cubic(spline_grids[0],
+                     coeff_sets[0],
+                     X[:,1:2])
+    diff = (d0[:,np.newaxis] - d0[np.newaxis,:]) / gradk.length_scale
+    tk = np.exp(-0.5 * diff**2)
+    print(np.mean(np.abs(gradk(X) - tk)))
+    print(np.mean(np.abs(res - evaluator.predict_from_desc(X, max_order = 1))))
+    print(np.mean(np.abs(res - aqrbf.scale[0] * tsp)))
+    print(evaluator.scale[0], scale[0], aqrbf.scale[0])
+    print(res[::1000])
+    print(tsp[::1000])
+    print("checked 1d")
+    print(np.mean(res - evaluator.predict_from_desc(X, max_order = 1)))
+    res += np.dot(alpha, aqrbf.scale[1] * en[1] * resg)
+    print(np.mean(np.abs(res - evaluator.predict_from_desc(X, max_order = 2))))
+    res += np.dot(alpha, aqrbf.scale[2] * en[2] * resg)
+    print(np.mean(np.abs(res - evaluator.predict_from_desc(X, max_order = 3))))
+
+    ytest = gpr.gp.predict(X)
+    ypred = evaluator.predict_from_desc(X)
+    test_y = gpr.xed_to_y(test_y, test_rho_data)
+    print(np.max(np.abs(ytest - ypred)))
+    print(np.max(np.abs(ytest - y)))
+    print(np.max(np.abs(y - ypred)))
+    print(np.linalg.norm(ytest - y))
+    print(np.linalg.norm(ypred - y))
+    print(ytest.shape)
+
+    if test_x is not None:
+        ytest = gpr.xed_to_y(gpr.predict(test_x, test_rho_data), test_rho_data)
+        ypred = gpr.xed_to_y(evaluator.predict(test_x, test_rho_data), test_rho_data)
+        print(np.max(np.abs(ytest - ypred)))
+        print(np.max(np.abs(ytest - test_y)))
+        print(np.max(np.abs(test_y - ypred)))
+        print()
+        print(np.linalg.norm(ytest - test_y))
+        print(np.mean(np.abs(ytest - test_y)))
+        print(np.mean(ytest - test_y))
+        print(np.linalg.norm(ypred - test_y))
+        print(np.mean(np.abs(ypred - test_y)))
+        print(np.mean(ypred - test_y))
+        print(np.linalg.norm(ypred - ytest))
+        print(np.mean(np.abs(ypred - ytest)))
+        print(np.mean(ypred - ytest))
+
+    return evaluator
+
+
+def get_mapped_gp_evaluator_corr(gpr, test_x = None, test_y = None,
+                                 test_rho_data = None, triples = False):
+    """
+    version a is for RBF(s) * ARBF(other desc) with X[:,0]=rho (used for exchange)
+    version b is for Linear(SCAN) * ARBF(other desc) with X[:,0]=rho
+    (used for correlation)
+    """
+    X = gpr.X[:,2:]
+    NFEAT = X.shape[1]
+    alpha = gpr.gp.alpha_
+    print("SHAPES", d0.shape, d1.shape)
+    y = gpr.y
+    print(gpr.gp.kernel_)
+    aqrbf = gpr.gp.kernel_.k1.k2
+    dims = []
+    if isinstance(aqrbf, RBF):
+        ndim, length_scale, scale = qarbf_args(gpr.gp.kernel_.k1.k1)
+        scale = np.array(scale)
+    else:
+        scale = aqrbf.scale
+    for i in range(NFEAT):
+        dims.append( get_dim(X[:,i], aqrbf.length_scale[i], density = 4, bound=bounds[i+1]) )
+    grid = [np.linspace(dims[i][0], dims[i][1], dims[i][2])\
+            for i in range(NFEAT)]
+    k0s = []
+    for i in range(NFEAT):
+        print(aqrbf.length_scale[i])
+        diff = (d1[:,i:i+1] - grid[i][np.newaxis,:]) / aqrbf.length_scale[i]
+        k0s.append(np.exp(-0.5 * diff**2))
+    funcps = []
+    spline_grids = []
+    ind_sets = []
+    for i in range(NFEAT):
+        print(i, dims[0], dims[i+1])
+        funcps.append(np.einsum('n,ni->i', alpha, k0s[i]))
+        spline_grids.append(UCGrid(dims[i]))
+        ind_sets.append((i,))
+    for i in range(NFEAT - 1):
+        for j in range(i+1, NFEAT):
+            k = np.einsum('ni,nj->nij', k0s[i], k0s[j])
+            funcps.append(np.einsum('n,nij->ij', alpha, k))
+            spline_grids.append(UCGrid(dims[i], dims[j]))
+            ind_sets.append((i,j))
+    if triples:
+        for i in range(NFEAT - 2):
+            for j in range(i+1, NFEAT - 1):
+                for k in range(j+1, NFEAT):
+                    k = np.einsum('ni,nj,nk->nijk', k0s[i], k0s[j], k0s[k])
+                    funcps.append(np.einsum('n,nijk->ijk', alpha, k))
+                    spline_grids.append(UCGrid(dims[i], dims[j], dims[k]))
+                    ind_sets.append((i,j,k))
+    print(spline_grids)
+    coeff_sets = []
+    for i in range(len(funcps)):
+        coeff_sets.append(filter_cubic(spline_grids[i], funcps[i]))
+    evaluator = Evaluator2(scale[1:], ind_sets, spline_grids, coeff_sets,
+                           gpr.y_to_xed, gpr.get_descriptors, gpr.num,
+                           const = scale[0])
     if not isinstance(aqrbf, RBF):
         return evaluator
 
