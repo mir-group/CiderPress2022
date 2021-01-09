5111d6eb75a462cb61befff58bd6b714d77aa270
kylebystrom <kylebystrom@g.harvard.edu>
2021-01-09 01:52:14 -0500

    update setup_qm9_dft scripts

--- scripts/setup_qm9_dft.py
+++ scripts/setup_qm9_dft.py
@@ -4,7 +4,7 @@ import ase.io
 
 fw_lst = []
 
-f = open('/n/holylfs/LABS/kozinsky_lab/Data/QM9/gdb9.sdf', 'r')
+f = open('/n/holystore01/LABS/kozinsky_lab/Lab/Data/QM9/gdb9.sdf', 'r')
 str_structs = f.read().split('$$$$\n', 300)
 f.close()
 from io import StringIO
@@ -16,10 +16,10 @@ structs = [(i+1, struct) for i, struct in enumerate(structs)]
 structs = [struct for struct in structs if sum(struct[1].numbers) % 2 == 0]
 structs = structs[:20]
 
-functional_list = ['pbe', 'scan', 'b3lyp']
+functional_list = ['wB97M_V']
 
 for i, struct in structs:
-    for basis in ['aug-cc-pvtz', 'cc-pcvtz']:
+    for basis in ['def2-qzvppd']:
         mol_id = 'qm9/%d-%s' % (i, struct.get_chemical_formula())
         for functional in functional_list:
             fw_lst.append(make_dft_firework(struct, mol_id, basis,
