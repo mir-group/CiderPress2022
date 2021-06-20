a4902788dfbbbe461e5e1f50e5b3411411700d92
Kyle Bystrom <kylebystrom@gmail.com>
2021-06-20 00:22:55 -0700

    removing more old files

--- train_gpy_iter2.sh
+++
@@ -1,2 +0,0 @@
-python mldftdat/scripts/glh_setup.py --functional HHGGA_GPY3_ITER1 --accdb train_coefs data_files/glh_mn_tr2.yaml \
-    --desc-name hhgga3.npy --noise 1e-5 --regression-method bayesian_lr ACCDB_TRAIN
